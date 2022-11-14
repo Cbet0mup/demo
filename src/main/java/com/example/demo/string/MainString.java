9b84290584d143aa6fc7d86e6a597727dfec8768
Павел Лютов <cbet0mup.pro@gmail.com>
2022-11-14 21:33:53 +0700

    srtghdtyhj

---
+++ src/main/java/com/example/demo/string/MainString.java
@@ -0,0 +1,22 @@
+package com.example.demo.string;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class MainString {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("C:\\java\\myProjects\\demo\\src\\main\\java\\com\\example\\demo\\string\\123.xml");
+        byte [] q = fileInputStream.readAllBytes();
+
+        ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(new String(q, StandardCharsets.UTF_8));
+
+        String s = StandardCharsets.UTF_8.decode(byteBuffer).toString();
+
+        System.out.println(s);
+
+
+    }
+}
